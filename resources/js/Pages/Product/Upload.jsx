4ed2b1e88fc8aa90534ccf4a00c5365deee1fe81
Roknuzzaman5546 <roknuzzamansojib78@gmail.com>
2024-07-27 18:50:33 +0600

    from code update

--- resources/js/Pages/Product/Upload.jsx
+++ resources/js/Pages/Product/Upload.jsx
@@ -50,51 +50,56 @@ const Upload = ({ auth }) => {
             user={auth.user}
             header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Product Upload</h2>}
         >
-            <h2>This is product upload file</h2>
+            <h2 className=' text-2xl font-bold mt-5 mb-5 text-center'>This is product upload file</h2>
 
             <form onSubmit={submit}>
-                <div>
-                    <InputLabel htmlFor="name" value="Name" />
-                    <TextInput
-                        id="name"
-                        type="name"
-                        name="name"
-                        value={data.name}
-                        className="mt-1 block w-full"
-                        autoComplete="username"
-                        isFocused={true}
-                        onChange={(e) => setData('name', e.target.value)}
-                    />
+                <div className=' w-1/2 p-10 mx-auto bg-lime-400 rounded-md'>
+                    <div>
+                        <InputLabel htmlFor="name" value="Name" />
+                        <TextInput
+                            id="name"
+                            type="name"
+                            name="name"
+                            value={data.name}
+                            placeholder={'Product Name'}
+                            className="mt-1 block w-full p-2 bg-blue-600"
+                            autoComplete="username"
+                            isFocused={true}
+                            onChange={(e) => setData('name', e.target.value)}
+                        />
+                    </div>
+                    <div className=' my-2'>
+                        <InputLabel htmlFor="brand" value="Brand" />
+                        <TextInput
+                            id="brand"
+                            type="brand"
+                            name="brand"
+                            value={data.brand}
+                            placeholder={'Product Brand'}
+                            className="mt-1 block w-full p-2 bg-blue-600"
+                            autoComplete="username"
+                            isFocused={true}
+                            onChange={(e) => setData('brand', e.target.value)}
+                        />
+                    </div>
+                    <div>
+                        <InputLabel htmlFor="country" value="Name" />
+                        <TextInput
+                            id="country"
+                            type="country"
+                            name="country"
+                            value={data.country}
+                            placeholder={'Product Country'}
+                            className="mt-1 block w-full p-2 text-black bg-blue-600"
+                            autoComplete="username"
+                            isFocused={true}
+                            onChange={(e) => setData('country', e.target.value)}
+                        />
+                    </div>
+                    <PrimaryButton className="ms-4 mt-4 mx-auto" disabled={processing}>
+                        Upload
+                    </PrimaryButton>
                 </div>
-                <div>
-                    <InputLabel htmlFor="brand" value="Name" />
-                    <TextInput
-                        id="brand"
-                        type="brand"
-                        name="brand"
-                        value={data.brand}
-                        className="mt-1 block w-full"
-                        autoComplete="username"
-                        isFocused={true}
-                        onChange={(e) => setData('brand', e.target.value)}
-                    />
-                </div>
-                <div>
-                    <InputLabel htmlFor="country" value="Name" />
-                    <TextInput
-                        id="country"
-                        type="country"
-                        name="country"
-                        value={data.country}
-                        className="mt-1 block w-full"
-                        autoComplete="username"
-                        isFocused={true}
-                        onChange={(e) => setData('country', e.target.value)}
-                    />
-                </div>
-                <PrimaryButton className="ms-4" disabled={processing}>
-                    Upload
-                </PrimaryButton>
             </form>
             <div onClick={handleCheck} className=' my-5'>
                 <PrimaryButton className="ms-4" disabled={processing}>
