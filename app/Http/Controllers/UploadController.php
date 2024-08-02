bceefc9051f9a8c82c574639f432624d820ffe61
Roknuzzaman5546 <roknuzzamansojib78@gmail.com>
2024-08-02 20:34:45 +0600

    some update

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -14,6 +14,7 @@ class UploadController extends Controller
     }
     public function productStore(Request $request)
     {
+        // dd($request->image);
         $array = [
             'sajib',
             'abir',
--- resources/js/Pages/Product/Upload.jsx
+++ resources/js/Pages/Product/Upload.jsx
@@ -15,11 +15,12 @@ const Upload = ({ auth }) => {
     } = useForm({
         name: '',
         brand: '',
+        image: '',
         country: '',
     });
 
-    console.log(data);
-    console.log(initialData);
+    // console.log(data);
+    // console.log(initialData);
     const submit = (e) => {
         e.preventDefault();
         post(route('product.post'));
@@ -50,10 +51,10 @@ const Upload = ({ auth }) => {
             user={auth.user}
             header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Product Upload</h2>}
         >
-            <h2 className=' text-2xl font-bold mt-5 mb-5 text-center'>This is product upload file</h2>
+            <h2 className=' text-2xl font-bold mt-5 mb-5 text-center text-blue-600 font-serif'>This is product upload file</h2>
 
             <form onSubmit={submit}>
-                <div className=' w-1/2 p-10 mx-auto bg-lime-400 rounded-md'>
+                <div className=' w-1/2 p-10 mx-auto bg-lime-400 rounded-md mt-10'>
                     <div>
                         <InputLabel htmlFor="name" value="Name" />
                         <TextInput
@@ -82,8 +83,22 @@ const Upload = ({ auth }) => {
                             onChange={(e) => setData('brand', e.target.value)}
                         />
                     </div>
+                    {/* <div className=' my-2'>
+                        <InputLabel htmlFor="image" value="Image" />
+                        <TextInput
+                            id="image"
+                            type="image"
+                            name="image"
+                            value={data.image}
+                            placeholder={'Product image'}
+                            className="mt-1 block w-full p-2 bg-blue-600"
+                            autoComplete="username"
+                            isFocused={true}
+                            onChange={(e) => setData('image', e.target.value)}
+                        />SS
+                    </div> */}
                     <div>
-                        <InputLabel htmlFor="country" value="Name" />
+                        <InputLabel htmlFor="country" value="Country" />
                         <TextInput
                             id="country"
                             type="country"
@@ -96,17 +111,34 @@ const Upload = ({ auth }) => {
                             onChange={(e) => setData('country', e.target.value)}
                         />
                     </div>
-                    <PrimaryButton className="ms-4 mt-4 mx-auto" disabled={processing}>
-                        Upload
-                    </PrimaryButton>
+                    <div>
+                        <InputLabel htmlFor="image" value="Image" />
+                        <TextInput
+                            id="image"
+                            type="file"
+                            name="image"
+                            value={data.image}
+                            placeholder={'Product Image URL'}
+                            className="mt-1 block w-full p-2 text-black bg-blue-600"
+                            autoComplete="username"
+                            isFocused={true}
+                            onChange={(e) => setData('image', e.target.value)}
+                        />
+                    </div>
+                    <div>
+                        <PrimaryButton className="mt-4 mx-auto" disabled={processing}>
+                            Upload
+                        </PrimaryButton>
+                    </div>
                 </div>
             </form>
-            <div onClick={handleCheck} className=' my-5'>
-                <PrimaryButton className="ms-4" disabled={processing}>
-                    check
-                </PrimaryButton>
+            <div>
+                {
+                    data?.image ?
+                        <img src={data?.image} alt="" /> :
+                        ''
+                }
             </div>
-            <button><a href={`uploadId/${auth?.user?.id}`}>click</a></button>
             <Head title="Dashboard" />
         </AuthenticatedLayout>
     );
