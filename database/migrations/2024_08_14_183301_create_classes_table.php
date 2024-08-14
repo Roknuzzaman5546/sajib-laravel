389322cce557354cda60eea3a8f667cebe1e6254
Roknuzzaman5546 <roknuzzamansojib78@gmail.com>
2024-08-15 01:05:09 +0600

    migration class student code added

--- database/migrations/2024_08_14_183301_create_classes_table.php
+++ database/migrations/2024_08_14_183301_create_classes_table.php
@@ -11,10 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('students', function (Blueprint $table) {
+        Schema::create('classes', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email');
+            $table->string('name')->nullable();
+            $table->string('email')->nullable();
             $table->timestamps();
         });
     }
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('students');
+        Schema::dropIfExists('classes');
     }
 };
